29cf2626339094e1ad7bffe9c32e095a55b0392f
Rumpelstilzkind <ne.mes@gmx.at>
2022-11-30 20:09:18 +0100

    impl. DowntimeController

--- src/main/java/at/fhtw/bic/uptimecalculatorbic/controller/DowntimeController.java
+++ src/main/java/at/fhtw/bic/uptimecalculatorbic/controller/DowntimeController.java
@@ -4,15 +4,16 @@ import jdk.jshell.spi.ExecutionControl;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.security.InvalidParameterException;
+
 public class DowntimeController {
     @GetMapping("/downtime/minutes")
-    public double calculateUptimeMinutes(@RequestParam double relative) {
+    public double calculateDowntimeMinutes(@RequestParam double relative) {
+        if (relative < 0.0 || relative > 100.0)
+            throw new InvalidParameterException();
         double minutesOfMonth = 60*24*30;
         double defineUptimeInPercent = relative /100;
-        return minutesOfMonth * defineUptimeInPercent; //uptime in minutes
+        return minutesOfMonth * defineUptimeInPercent; //downtime in minutes
     }
 
-    public double calculateDowntimeMinutes(double v) {
-        return 0.0;
-    }
 }
